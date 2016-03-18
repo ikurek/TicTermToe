4c852c067038fd467b6f4cd80945a69f540d0744
Igor Kurek <igorkurek96@gmail.com>
2016-03-19 00:31:31 +0100

    Drawing board added
    Added drawing a nice board, separate cpp file

--- drawboardplease.cpp
+++ drawboardplease.cpp
@@ -6,5 +6,9 @@ using namespace std;
 
 void drawboard(char board[3][3])
 {
-    printf("%c", board[2][2]);
+    printf(" %c | %c | %c  \n", board[0][0], board[0][1], board[0][2]);
+    printf("----------- \n");
+    printf(" %c | %c | %c  \n", board[1][0], board[1][1], board[1][2]);
+    printf("----------- \n");
+    printf(" %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
 }
